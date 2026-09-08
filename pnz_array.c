8fedc51b90ff958a88be953bfa4c3ef51af854ba
Amrit-Malik <amritmalik0907@gmail.com>
2026-09-08 15:42:49 +0530

    Added two new files

---
+++ pnz_array.c
@@ -0,0 +1,32 @@
+/* Q60: Count positive, negative, and zero elements in an array.
+
+Sample Test Cases:
+Input 1:
+5
+-1 0 1 2 -2
+Output 1:
+Positive=2, Negative=2, Zero=1
+
+*/
+#include<stdio.h>
+int main()
+{
+    int i, n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    int pos = 0, neg = 0, zero = 0;
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        if(arr[i] > 0)
+        pos++;
+        else if(arr[i] < 0)
+        neg++;
+        else
+        zero++;
+    }
+    printf("Positive = %d, Negative = %d, Zero = %d\n", pos, neg, zero);
+    return 0;
+}
