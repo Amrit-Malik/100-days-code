4a86acede113787de9010749cac08c1cae7e66cc
Amrit-Malik <amritmalik0907@gmail.com>
2026-09-21 17:23:16 +0530

    Added two new files

---
+++ Reverse_String.c
@@ -0,0 +1,27 @@
+// Q85: Reverse a string.
+
+/*
+Sample Test Cases:
+Input 1:
+abcd
+Output 1:
+dcba
+
+*/
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    char str[100], rev[100];
+    int i, len;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    len = strlen(str);
+    for(i = 0; i < len; i++) 
+    {
+        rev[i] = str[len - 1 - i];
+    }
+    rev[len] = '\0';
+    printf("Reversed string: %s", rev);
+    return 0;
+}
